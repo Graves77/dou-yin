b8e70f44aa6872362759f0cca7f0a77f182ebf2e
Graves77 <870519251@qq.com>
2023-02-07 11:09:32 +0800

    基本的初始化

---
+++ apps/favorite/rpc/internal/svc/servicecontext.go
@@ -0,0 +1,13 @@
+package svc
+
+import "awesomeProject/dou-yin/apps/favorite/rpc/internal/config"
+
+type ServiceContext struct {
+	Config config.Config
+}
+
+func NewServiceContext(c config.Config) *ServiceContext {
+	return &ServiceContext{
+		Config: c,
+	}
+}
