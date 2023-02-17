4c3a2e7cd68cbc27c01303c171a4212bb52b6e0b
Graves77 <870519251@qq.com>
2023-02-17 10:48:22 +0800

    video的rpc模块基本完成

--- video/cmd/api/internal/middleware/uploadfilemiddleware.go
+++ video/cmd/api/internal/middleware/uploadfilemiddleware.go
@@ -11,9 +11,6 @@ func NewUploadFileMiddleware() *UploadFileMiddleware {
 
 func (m *UploadFileMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		// TODO generate middleware implement function, delete after code implementation
-
-		// Passthrough to next handler if need
 		next(w, r)
 	}
 }
--- video/cmd/api/internal/svc/servicecontext.go
+++ video/cmd/api/internal/svc/servicecontext.go
@@ -3,17 +3,25 @@ package svc
 import (
 	"awesomeProject/dou-yin/video/cmd/api/internal/config"
 	"awesomeProject/dou-yin/video/cmd/api/internal/middleware"
+	"awesomeProject/dou-yin/video/cmd/rpc/publishvideo"
+	"awesomeProject/dou-yin/video/cmd/rpc/types/video"
+	"github.com/zeromicro/go-zero/core/stores/redis"
 	"github.com/zeromicro/go-zero/rest"
+	"github.com/zeromicro/go-zero/zrpc"
 )
 
 type ServiceContext struct {
 	Config     config.Config
 	UploadFile rest.Middleware
+	Redis      *redis.Redis
+	VideoRpc   video.PublishVideoClient
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config:     c,
 		UploadFile: middleware.NewUploadFileMiddleware().Handle,
+		Redis:      redis.New(c.Redis.Host),
+		VideoRpc:   publishvideo.NewPublishVideo(zrpc.MustNewClient(c.VideoRpc)),
 	}
 }
