58374ae8236a0f9c4843e55f57fce70e0e9b10d1
Graves77 <870519251@qq.com>
2023-02-23 23:45:59 +0800

    代码改进

--- apps/api/internal/middleware/uploadfilemiddleware.go
+++ apps/api/internal/middleware/uploadfilemiddleware.go
@@ -1,9 +1,6 @@
 package middleware
 
 import (
-	"awesomeProject/dou-yin/apps/video/cmd/rpc/types/video"
-	"fmt"
-	"github.com/gin-gonic/gin"
 	"net/http"
 )
 
@@ -14,15 +11,8 @@ func NewUploadFileMiddleware() *UploadFileMiddleware {
 	return &UploadFileMiddleware{}
 }
 
-func (m *UploadFileMiddleware) Handle(c *gin.Context) http.HandlerFunc {
+func (m *UploadFileMiddleware) Handle(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		file, header, _ := c.Request.FormFile("data")
-		video.SaveFile(file, header)
-		token := c.PostForm("token")
-		title := c.PostForm("title")
-		fmt.Println(token, title)
-		//service.UploadService(param1, param2, filename)
-		c.String(http.StatusOK, "File uploaded successfully")
-
+		next(w, r)
 	}
 }
